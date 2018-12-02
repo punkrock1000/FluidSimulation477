c0ab26acf81dd6df681666ab33e949f71efd97a0
punkrock1000 <antoineracinegingras@hotmail.com>
2018-12-01 22:03:58 -0500

    Added Open GL functionality to project

---
+++ COMP477_A1/src/WaterParticle.cpp
@@ -0,0 +1,29 @@
+#include "WaterParticle.h"
+
+WaterParticle::WaterParticle()
+{
+	j = 0.05;//particle mass
+	r << -1, -1, -1;
+	p = 0.005;//particle density
+	//a = new WaterParticle[8];//neighbours 0-7
+	global_Index = -1;
+}
+
+void WaterParticle::setPosition(Vector3f v) {
+	r(0) = v(0);
+	r(1) = v(1);
+	r(2) = v(2);
+}
+
+void WaterParticle::draw()
+{
+	glPushMatrix();
+	glColor3f(0, 0.5, 1);
+	glTranslatef(r(0), r(1), r(2));
+	glutSolidSphere(rad, nSlices, nStacks);
+	glPopMatrix();
+}
+
+WaterParticle::~WaterParticle()
+{
+}
